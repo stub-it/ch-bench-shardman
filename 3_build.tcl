6a4ae4b42a2ca0fd7b9df035b70b13c8e49ee409
Pavel Safonov <p.safonov@postgrespro.ru>
2021-11-29 11:14:38 +0300

    add script

---
+++ 3_build.tcl
@@ -0,0 +1,27 @@
+#!/bin/tclsh
+puts "SETTING CONFIGURATION"
+global complete
+proc wait_to_complete {} {
+global complete
+set complete [vucomplete]
+if {!$complete} { after 5000 wait_to_complete } else { exit }
+}
+dbset db pg
+loadscript
+diset connection pg_host $env(PGHOST)
+diset connection pg_port $env(PGPORT)
+diset tpcc pg_dbase $env(PGDATABASE)
+diset tpcc pg_user $env(PGUSER)
+diset tpcc pg_superuser $env(PGUSER)
+diset tpcc pg_defaultdbase $env(PGDATABASE)
+diset tpcc pg_pass $env(PGPASSWORD)
+diset tpcc pg_superuserpass $env(PGPASSWORD)
+diset tpcc pg_storedprocs true
+diset tpcc pg_num_vu 3
+diset tpcc pg_count_ware 10
+loadscript
+print dict
+buildschema
+#wait_to_complete
+#vwait forever
+waittocomplete
